66f80d5a5e87af2537b871d96b7a83fce710a991
Jebakumar <jebakumar.6208@gmail.com>
2025-01-29 11:59:14 +0530

    updated

---
+++ server.js
@@ -0,0 +1,15 @@
+// Load the dotenv package
+require('dotenv').config();
+
+// Access environment variables
+const config = {
+  apiKey: process.env.API_KEY,
+  serverSecret: process.env.SERVER_SECRET,
+  isKalvian: process.env.IS_KALVIAN === 'true',
+};
+
+// Log the environment variables
+console.log("Configuration:");
+console.log(`API Key: ${config.apiKey}`);
+console.log(`Server Secret: ${config.serverSecret}`);
+console.log(`Is Kalvian? ${config.isKalvian}`);
